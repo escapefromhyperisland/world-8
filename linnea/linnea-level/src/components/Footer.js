82328ed7e9120dc3158cf8350b738e27db85c17c
lingi524 <lingi524@student.liu.se>
2021-05-25 21:49:19 +0200

    adde image and footer file

---
+++ linnea/linnea-level/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Footer () {
+   return ( <div>
+        <p>Made by Linnéa Gistrand for the project Escape from Hyper Island</p>
+        <p><a href='https://www.freepik.com/vectors/infographic'>Infographic vector created by pikisuperstar - www.freepik.com</a></p>
+    </div>)
+}
+
+export default Footer
