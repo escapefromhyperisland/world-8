05fe133d585145e5395732c242f40ae8e843b678
lingi524 <lingi524@student.liu.se>
2021-05-26 10:59:02 +0200

    Update Footer.js
    Test

--- linnea/linnea-level/src/components/Footer.js
+++ linnea/linnea-level/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Footer () {
    return ( <div>
-        <p>Made by Linnéa Gistrand for the project Escape from Hyper Island</p>
+        <p>Made by Linnéa Gistrand for the project Escape from Hyper Island - Test</p>
         <p><a href='https://www.freepik.com/vectors/infographic'>Infographic vector created by pikisuperstar - www.freepik.com</a></p>
     </div>)
 }
