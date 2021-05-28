15de54007811ba8a772e8120fae9d11f3f91acad
lingi524 <lingi524@student.liu.se>
2021-05-28 17:05:40 +0200

    Changes value of one of the texts

--- linnea/linnea-level/src/components/Textbox2.js
+++ linnea/linnea-level/src/components/Textbox2.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Textbox2Image from "./images/game-screen.png";
 
 
@@ -8,17 +8,29 @@ function Textbox2 () {
     const [alternativeText2, setAlternativeText2] = useState("Go to the office");
     const [alternativeText3, setAlternativeText3] = useState("Go to the art hall");
 
+    const textAlternatives = [
+        { id: 1, value: "Go back to the hallway" },
+        { id: 2, value: "Look at the calendar" },
+        { id: 3, value: "Take pen?" }
+      ];
 
+    // useEffect(() => {
+    //     // Should not ever set state during rendering, so do this in useEffect instead.
+    //     setAlternativeText1(textAlternatives);
+    //   }, []);
 
    return ( 
+       
    <div id="TextBox2">
+
         <img src={Textbox2Image} alt="Textbox nr 2"></img>
         <div id="text-TextBox2">
-            <li>{alternativeText1}</li>
+            <li onClick={()=> setAlternativeText1(textAlternatives[0].value)}>{alternativeText1}</li>
             <li>{alternativeText2}</li>
             <li>{alternativeText3}</li>
         </div>
-    </div>)
+    </div>
+    )
 }
 
 export default Textbox2
