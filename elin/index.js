35bb1eec23b75d16a685717a2488bd60087d93d6
elinordin <elin.evelina@live.se>
2021-05-30 13:00:22 +0200

    make click event work on dots (with keyboard commented out)

--- elin/index.js
+++ elin/index.js
@@ -1,28 +1,22 @@
 AFRAME.registerComponent('keyboard-functions', {
 
     init: function () {
-
-        let isPasswordCorrect = false
         
+        let isPasswordCorrect = false
+
         // for referencing issues
         let self = this;
 
-        // set up event listeners
-
         // input event triggered when user presses enter
         this.el.addEventListener('superkeyboardinput', function (event) {
-            let text = event.detail.value;
-
             // text also accessible via: self.el.getAttribute("super-keyboard")["value"]
-
-            console.log("Input: " + text);
+            let text = event.detail.value;
 
             // clear the input bar (since keyboard is not disappearing)
             self.el.setAttribute("super-keyboard", "value", "");
 
             // handle password
-            if(text === "correct") {
-                console.log("You saved the world!")
+            if (text === "correct") {
                 document.querySelector("#button").setAttribute("gltf-model", "/assets/models/green-button.glb")
                 isPasswordCorrect = true
             } else {
@@ -35,7 +29,6 @@ AFRAME.registerComponent('keyboard-functions', {
         // dismiss event triggered when user closes keyboard
         this.el.addEventListener('superkeyboarddismiss', function (event) {
             // repurpose close functionality to clear all input text without submitting
-            console.log("Input cleared.");
             self.el.setAttribute("super-keyboard", "value", "");
         });
     },
@@ -46,4 +39,15 @@ AFRAME.registerComponent('keyboard-functions', {
             this.el.object3D.visible = true;
     }
 
-});
+})
+
+AFRAME.registerComponent('dot-function', {
+
+    init: function () {
+        this.el.addEventListener('click', function (event) {
+            console.log("clicked")
+            this.setAttribute('material', 'color', 'black');
+        })
+    }
+
+})
